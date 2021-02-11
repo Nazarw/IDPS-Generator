c4249d31bc4cdd7d817e5d376538fca919c71348
1337.xyz <52249933+Nazarw@users.noreply.github.com>
2021-02-11 09:53:58 +0500

    Update Duplicates Remove.py

--- Duplicates Remove.py
+++ Duplicates Remove.py
@@ -1,5 +1,5 @@
 lines_seen = set()
-with open("sniped_cleaned.txt", "w") as output_file:
+with open("idps_cleaned.txt", "w") as output_file:
 	for each_line in open("idps.txt", "r"):
 	    if each_line not in lines_seen:
 	        output_file.write(each_line)
